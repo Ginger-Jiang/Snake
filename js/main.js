833475011d4355f6e95ef7d92c9edecd2102abc4
Ginger <ziqingnanyi@gmail.com>
2019-07-10 21:39:36 +0800

    add Random Food

--- js/main.js
+++ js/main.js
@@ -8,8 +8,38 @@ window.onload = function () {
     win.Random = new Random()
   })(window);
   // 产生小方块对象
-  // 食物的构造函数
-  // 初始化小方块效果与位置的方法
-  // 设置小方块的样式
-  // 随机位置
+  (function (win) {
+    var map = document.querySelector('.map')
+    // 食物的构造函数
+    function Food (width, height, color) {
+      this.width = width || 20
+      this.height = height || 20
+      this.color = color || 'green'
+      // 坐标
+      this.x = 0
+      this.y = 0
+      this.ele = document.createElement('div')
+    }
+    // 初始化小方块效果与位置的方法
+    Food.prototype.init = function (map) {
+      // 设置小方块的样式
+      this.ele.style.width = this.width + 'px'
+      this.ele.style.height = this.height + 'px'
+      this.ele.style.background = this.color
+      this.ele.style.position = 'absolute'
+      map.appendChild(this.ele)
+      this.render(map)
+    }
+    // 随机位置
+    Food.prototype.render = function (map) {
+      this.x = Random.getRandom(0, map.offsetWidth / this.width) * this.width
+      this.y = Random.getRandom(0, map.offsetHeight / this.height) * this.height
+      this.ele.style.left = this.x + 'px'
+      this.ele.style.top = this.y + 'px'
+    }
+
+    // 实例化对象
+    var food = new Food()
+    food.init(map)
+  })(window)
 }
