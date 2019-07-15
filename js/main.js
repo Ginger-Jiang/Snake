7706a4de2c4f831845787b3c41389b5ecf52ca66
Ginger <ziqingnanyi@gmail.com>
2019-07-15 23:43:03 +0800

    add elements Array

--- js/main.js
+++ js/main.js
@@ -1,45 +1,37 @@
-window.onload = function () {
-  // 产生随机数对象
-  (function (win) {
-    function Random () { }
-    Random.prototype.getRandom = function (min = 5, max = 10) {
-      return Math.floor(Math.random() * (max - min) + min)
-    }
-    win.Random = new Random()
-  })(window);
-  // 产生小方块对象
-  (function (win) {
-    var map = document.querySelector('.map')
-    // 食物的构造函数
-    function Food (width, height, color) {
-      this.width = width || 20
-      this.height = height || 20
-      this.color = color || 'green'
-      // 坐标
-      this.x = 0
-      this.y = 0
-      this.ele = document.createElement('div')
-    }
-    // 初始化小方块效果与位置的方法
-    Food.prototype.init = function (map) {
-      // 设置小方块的样式
-      this.ele.style.width = this.width + 'px'
-      this.ele.style.height = this.height + 'px'
-      this.ele.style.background = this.color
-      this.ele.style.position = 'absolute'
-      map.appendChild(this.ele)
-      this.render(map)
-    }
-    // 随机位置
-    Food.prototype.render = function (map) {
-      this.x = Random.getRandom(0, map.offsetWidth / this.width) * this.width
-      this.y = Random.getRandom(0, map.offsetHeight / this.height) * this.height
-      this.ele.style.left = this.x + 'px'
-      this.ele.style.top = this.y + 'px'
-    }
+(function (win) {
+  var map = document.querySelector('.map')
+  var elements = [] // 食物数组
+  // 随机坐标
+  function Random (max, min) {
+    return Math.floor(Math.random() * (max - min) + min)
+  }
+  // 食物有宽高颜色坐标
+  function Food (width, height, color, x, y) {
+    this.width = width || 20
+    this.height = height || 20
+    this.color = color || 'green'
+    this.x = x || 0
+    this.y = y || 0
+    this.ele = document.createElement('div')
+  }
+  Food.prototype.init = function () {
+    this.ele.style.width = this.width + 'px'
+    this.ele.style.height = this.height + 'px'
+    this.ele.style.position = 'absolute'
+    this.ele.style.backgroundColor = this.color
+    map.appendChild(this.ele)
+    elements.push(this.ele)
+    this.random()
+  }
+  Food.prototype.random = function () {
+    this.x = Random(map.offsetWidth / this.width, 0) * this.width + 'px'
+    this.y = Random(map.offsetHeight / this.height, 0) * this.height + 'px'
+    this.ele.style.left = this.x
+    this.ele.style.top = this.y
+  }
 
-    // 实例化对象
-    var food = new Food()
-    food.init(map)
-  })(window)
-}
+
+  win.Food = Food
+}(window));
+
+new Food().init()
